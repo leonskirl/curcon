fc6f86addbb9af7c8cc46d9364a234237e1004b5
leonskirl <134922271+leonskirl@users.noreply.github.com>
2023-06-05 15:07:23 +0800

    Update currency_converter.py

--- currency_converter.py
+++ currency_converter.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 import requests
 
 def currency_converter(amount, from_currency, to_currency):
-    api_key = '74123f01865d98efbd09dd75'  # Replace with your API key
+    api_key = ''  # replace with your API key
     url = f'https://api.exchangerate-api.com/v4/latest/{from_currency}'
 
     response = requests.get(url)
@@ -33,11 +33,11 @@ def convert_button_clicked():
     if converted_amount:
         messagebox.showinfo('Conversion Result', f'{amount} {from_currency} is equal to {converted_amount} {to_currency}')
 
-# Create the main window
+# creates the main window
 window = tk.Tk()
 window.title('Currency Converter')
 
-# Create the input fields and labels
+# creates the input fields and labels
 label_amount = tk.Label(window, text='Amount:')
 label_amount.grid(row=0, column=0, padx=10, pady=10)
 entry_amount = tk.Entry(window)
@@ -53,11 +53,11 @@ label_to_currency.grid(row=2, column=0, padx=10, pady=10)
 entry_to_currency = tk.Entry(window)
 entry_to_currency.grid(row=2, column=1, padx=10, pady=10)
 
-# Create the Convert button
+# creates the convert button
 convert_button = tk.Button(window, text='Convert', command=convert_button_clicked)
 convert_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
 
-# Start the main GUI event loop
+# starts the main GUI event loop
 window.mainloop()
 
 
